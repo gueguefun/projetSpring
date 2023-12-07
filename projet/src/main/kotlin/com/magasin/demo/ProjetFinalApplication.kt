128c7a47904123aa5f08b914f81067cc1d4a893a
E211222P <guewenguyomard@gmail.com>
2023-12-07 08:41:34 +0100

    Push initial
    Voici le premier push alexANDRE

---
+++ projet/src/main/kotlin/com/magasin/demo/ProjetFinalApplication.kt
@@ -0,0 +1,11 @@
+package com.magasin.demo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ProjetFinalApplication
+
+fun main(args: Array<String>) {
+	runApplication<ProjetFinalApplication>(*args)
+}
