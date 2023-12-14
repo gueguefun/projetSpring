033569d7c43d9dfc69e1cf0f64c6d522b89595bf
E211222P <guewenguyomard@gmail.com>
2023-12-14 08:28:28 +0100

    Update la bd en mémoire
    Oupsi j'avais oublié ça

--- Panier/src/main/kotlin/Projet/Panier/repository/PanierDbMemory.kt
+++ Panier/src/main/kotlin/Projet/Panier/repository/PanierDbMemory.kt
@@ -1,21 +1,42 @@
 package Projet.Panier.repository
 
+import Projet.Panier.domain.Article
 import Projet.Panier.domain.Panier
+import Projet.Panier.repository.entity.ArticleEntity
+import Projet.Panier.repository.entity.PanierEntity
 import java.net.HttpURLConnection
 import java.net.URL
 
-/*
+
 class PanierDbMemory : PanierRepository {
 
     private val map = mutableMapOf<String, Panier>()
 
-    override fun create(panier: Panier): Result<Panier> {
+    override fun create(id : String): Result<Panier> {
 
-        val previous = map.putIfAbsent(panier.userEmail, panier)
-        return if (previous == null) {
-            Result.success(panier)
+        if(map.containsKey(id)){
+            return Result.failure(Exception("Panier already exists"))
         } else {
-            Result.failure(Exception("Panier already exists"))
+            val url = "http://localhost:8080/users/"
+            val connectUrl = url + id
+            val connection = URL(connectUrl).openConnection() as HttpURLConnection
+            connection.requestMethod = "GET"
+
+            return try {
+                //Récuparation de la réponse
+                val response = connection.responseCode
+
+                if (response != 200) {
+                    if (response == 404) throw Exception("User not found")
+                    Result.failure(Exception("User not found"))
+                } else {
+                    val panier = PanierEntity(id, mutableListOf())
+                    val saved = map.putIfAbsent(id, panier.asPanier())
+                    Result.success(saved!!)
+                }
+            } finally {
+                connection.disconnect()
+            }
         }
     }
 
@@ -35,7 +56,7 @@ class PanierDbMemory : PanierRepository {
     override fun delete(id: String): Panier? = map.remove(id)
 
     override fun validate(id: String): Boolean {
-        val url = "http://localhost:8081/articles/quantity/"
+        var url = "http://localhost:8081/articles/quantity/"
         var validate = true
         for (item in map[id]!!.items) {
             val quantity = item.quantite.toString()
@@ -59,10 +80,73 @@ class PanierDbMemory : PanierRepository {
             }
         }
         if (validate) {
-            //update la quantité sur la base de donnée article
+            url = "http://localhost:8081/articles/admin/quantity/"
+            for (item in map[id]!!.items) {
+                val quantity = item.quantite.toString()
+                val connectUrl = url + item.articleId + "/" + quantity
+                val connection = URL(connectUrl).openConnection() as HttpURLConnection
+
+                connection.requestMethod = "PUT"
+
+                try {
+                    //Récuparation de la réponse
+                    val response = connection.responseCode
+
+                    if (response != 200) {
+                        validate = false
+                        if (response == 404) throw Exception("Article not found")
+                        if (response == 409) throw Exception("Quantity not available")
+                    }
+
+                } finally {
+                    connection.disconnect()
+                }
+            }
         }
         return validate
     }
+
+    override fun addArticle(id: String, articleId: Int, quantite: Int): Result<Panier> {
+        val url = "http://localhost:8081/articles/"
+        val connectUrl = url + articleId
+        val connection = URL(connectUrl).openConnection() as HttpURLConnection
+
+        connection.requestMethod = "GET"
+
+        return try {
+            //Récuparation de la réponse
+            val response = connection.responseCode
+
+            if (response != 200) {
+
+                Result.failure(Exception("Article not found"))
+            } else {
+                val panier = map[id]!!
+                val article = panier.items.find { it.articleId == articleId }
+                if (article != null) {
+                    article.quantite += quantite
+                } else {
+                    panier.items.add(Article(articleId, quantite))
+                }
+                Result.success(panier)
+            }
+        } finally {
+            connection.disconnect()
+        }
+    }
+
+    override fun deleteArticle(id: String, articleId: Int): Result<Panier> {
+        return if (map.containsKey(id)) {
+            val panier = map[id]!!
+            val article = panier.items.find { it.articleId == articleId }
+            if (article != null) {
+                panier.items.remove(article)
+            }
+            Result.success(panier)
+        } else {
+            Result.failure(Exception("Panier not in DB"))
+        }
+    }
 }
 
-*/
+
